a0dbe442e26d85b6ff5a37778db89a6cde7d5d49
John Rayes <live627@gmail.com>
2015-03-10 17:54:01 -0700

    Add hook class (no jokes about hookers, please...)

---
+++ src/Hooks.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ModHelper;
+
+/**
+ * @package ModHelper
+ * @since 1.0
+ */
+class Hooks
+{
+	protected $hooks = array();
+	protected $collection;
+
+	public function execute($add)
+	{
+		foreach ($collection as list ($hook, $function, $permanent)) {
+			if ($add) {
+				add_integration_function($hook, $function, $permanent);
+			} else {
+				remove_integration_function($hook, $function, $permanent);
+			}
+		}
+	}
+
+	protected function add($hook, $function, $permanent = true)
+	{
+		$this->collection>addValue($hook, $function, $permanent);
+	}
+
+	public function __construct()
+	{
+		$collection = new Collection();
+	}
+
+}
