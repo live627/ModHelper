b61289be7b6a31456556802e6aa7c347c124c8d4
John Rayes <live627@gmail.com>
2015-04-05 13:30:14 -0700

    Add linktree collection

---
+++ src/live627/Helper/Linktree.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace ModHelper;
+
+/**
+ * @package ModHelper
+ * @since 1.0
+ */
+class Linktree
+{
+	protected $collection = array();
+
+	public function execute()
+	{
+		global $context;
+
+		foreach ($this->collection as $item) {
+			$context['linktree'][] = $item;
+		}
+	}
+
+	public function add($name, $url = null, $before = null, $after = null, $first = false)
+	{
+		$item = array(
+			'name' => $name,
+		);
+		if ($url !== null)
+			$item['url'] = $url;
+		if ($before !== null)
+			$item['extra_before'] = $before;
+		if ($after !== null)
+			$item['extra_after'] = $after
+
+		$this->collection->addValue($item);
+
+		return $this;
+	}
+
+	public function __construct()
+	{
+		$this->collection = new Collection();
+
+		return $this;
+	}
+
+}
