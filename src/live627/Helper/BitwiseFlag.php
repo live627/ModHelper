c586cc7906fa7a4af1cdaf9f67f3a9bb91f7b498
John Rayes <live627@gmail.com>
2015-05-30 18:23:11 -0700

    BitwiseFlag

---
+++ src/live627/Helper/BitwiseFlag.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace ModHelper;
+
+/**
+ * @package ModHelper
+ * @since 1.0
+ */
+
+abstract class BitwiseFlag
+{
+	private $flags;
+
+	/*
+	 * Note: these functions are protected to prevent outside code
+	 * from falsely setting BITS.
+	 */
+
+	/*
+	 * Returns the stored bits.
+	 *
+	 * @access protected
+	 * @return int
+	 */
+	protected function __construct()
+	{
+		return $this->flags;
+	}
+
+	protected function isFlagSet($flag)
+	{
+		return (($this->flags & $flag) == $flag);
+	}
+
+	protected function setFlag($flag, $value)
+	{
+		if ($value) {
+			$this->flags |= $flag;
+		} else {
+			$this->flags &= ~$flag;
+		}
+	}
+}
+
+?>
