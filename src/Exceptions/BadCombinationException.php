39da511604a3aebb314c183bda905c9c248ee815
John Rayes <live627@gmail.com>
2015-07-03 19:09:20 -0700

    BadCombinationException

---
+++ src/Exceptions/BadCombinationException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ModHelper\Exceptions;
+
+/**
+ * @copyright Copyright (c) 2015 John Rayes
+ * @license http://opensource.org/licenses/MIT MIT
+ *
+ * @package ModHelper
+ * @version 1.0
+ */
+class BadCombinationException extends ValidationException {}
+
