2e79fa2407a9d77a984ce453b7246bcf77782276
John Rayes <live627@gmail.com>
2015-07-03 18:10:57 -0700

    throw BadFunctionCallException

--- src/Database.php
+++ src/Database.php
@@ -17,18 +17,20 @@ class Database
 	 * @param string $name The name (or key) of the $smcFunc you are calling.
 	 * @param string $arguments This is an array of all arguments passed to the method.
 	 * @return mixed The $smcFunc return value or false if not found.
-	 * @since 3.0
-	 * @version 3.0
+	 * @throws BadFunctionCallException
+	 * @since 1.0
+	 * @version 1.1
 	 */
 	public static function __callStatic($name, $arguments)
 	{
 		global $smcFunc;
 		if (isset($smcFunc[$name = 'db_' . $name])) {
+			if (!is_callable($smcFunc[$name])) {
+				throw new \BadFunctionCallException('Function $smcFunc[' . $name . '] is not callable');
+			}
 			return call_user_func_array($smcFunc[$name], $arguments);
 		}
 
 		return false;
 	}
 }
-
-?>
