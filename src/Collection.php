d43d88a8512f9c23f160833229628c1864fd3ffd
John Rayes <live627@gmail.com>
2015-07-30 06:53:44 -0700

    implement,ArrayAccess

--- src/Collection.php
+++ src/Collection.php
@@ -6,9 +6,33 @@ namespace ModHelper;
  * @package ModHelper
  * @since 1.0
  */
-class Collection implements \IteratorAggregate
+class Collection implements \IteratorAggregate, \ArrayAccess
 {
 	private $items = array();
+
+	public function offsetSet($offset, $value)
+	{
+		if (is_null($offset)) {
+			$this->items[] = $value;
+		} else {
+			$this->items[$offset] = $value;
+		}
+	}
+
+	public function offsetExists($offset)
+	{
+		return isset($this->items[$offset]);
+	}
+
+	public function offsetUnset($offset)
+	{
+		unset($this->items[$offset]);
+	}
+
+	public function offsetGet($offset)
+	{
+		return isset($this->items[$offset]) ? $this->items[$offset] : null;
+	}
 
 	public function addValue($item)
 	{
