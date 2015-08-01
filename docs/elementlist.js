0e249631d6fa820ed3c70ddf6e05a1824b3d6f46
John Rayes <live627@gmail.com>
2015-08-01 16:19:52 -0700

    apigen

---
+++ docs/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","ModHelper\\A"],["c","ModHelper\\BitwiseFlag"],["c","ModHelper\\Collection"],["c","ModHelper\\Database"],["c","ModHelper\\Editor"],["c","ModHelper\\Exceptions\\BadCombinationException"],["c","ModHelper\\Exceptions\\MissingDataException"],["c","ModHelper\\Exceptions\\ValidationException"],["c","ModHelper\\Hooks"],["c","ModHelper\\Linktree"],["c","ModHelper\\Menu"],["c","ModHelper\\Nonce"],["c","ModHelper\\Psr4AutoloaderClass"],["c","ModHelper\\SingletonTrait"],["c","ModHelper\\Verify"]];
