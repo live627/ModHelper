83627e119cfe1e9c92405b0103976a2b4e7f2727
John Rayes <live627@gmail.com>
2016-03-17 21:47:58 -0700

    Update LinktreeTest.php

--- tests/LinktreeTest.php
+++ tests/LinktreeTest.php
@@ -21,33 +21,4 @@ class LinktreeTest extends \PHPUnit_Framework_TestCase
         );
 
     }
-
-    public function testExistingLink()
-    {
-        $expect = array(
-            'Foo',
-            '/vendor/foo'
-        );
-        $this->assertContains($expect, $this->l->collection);
-
-        $expect = array(
-            'BarDoom',
-            '/vendor/foo.bardoom'
-        );
-        $this->assertContains($expect, $this->l->collection);
-    }
-
-    public function testMissingLink()
-    {
-        $expect = array(
-            'Baz Dib',
-            '/vendor/baz.dib'
-        );
-        $this->assertNotContains($expect, $this->l->collection);
-    }
-
-    public function testLinkCount()
-    {
-        $this->assertCount(2, $this->l->collection);
-    }
 }
