7632d8430506b49bc6ce0ffaf903c1c9cb46c859
John Rayes <live627@gmail.com>
2016-03-17 22:00:11 -0700

    Update LinktreeTest.php

--- tests/LinktreeTest.php
+++ tests/LinktreeTest.php
@@ -2,13 +2,21 @@
 
 namespace ModHelper\Tests;
 
+class MockLinktree extends \ModHelper\Linktree
+{
+    public function getLinks()
+    {
+        return $this->collection;
+    }
+}
+
 class LinktreeTest extends \PHPUnit_Framework_TestCase
 {
     protected $l;
 
     protected function setUp()
     {
-        $this->l = new \ModHelper\Linktree();
+        $this->l = new MockLinktree;
 
         $this->l->add(
             'Foo',
@@ -20,5 +28,34 @@ class LinktreeTest extends \PHPUnit_Framework_TestCase
             '/vendor/foo.bardoom'
         );
 
+    }
+
+    public function testExistingLink()
+    {
+        $expect = array(
+            'Foo',
+            '/vendor/foo'
+        );
+        $this->assertContains($expect, $this->l->getLinks());
+
+        $expect = array(
+            'BarDoom',
+            '/vendor/foo.bardoom'
+        );
+        $this->assertContains($expect, $this->l->getLinks());
+    }
+
+    public function testMissingLink()
+    {
+        $expect = array(
+            'Baz Dib',
+            '/vendor/baz.dib'
+        );
+        $this->assertNotContains($expect, $this->l->getLinks());
+    }
+
+    public function testLinkCount()
+    {
+        $this->assertCount(2, $this->l->getLinks());
     }
 }
