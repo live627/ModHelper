c2a6bbd3e3f5209d7f0f33f92eea456b62eb91ca
John Rayes <live627@gmail.com>
2016-03-17 23:49:42 -0700

    Update BitwiseFlagTest.php

--- tests/BitwiseFlagTest.php
+++ tests/BitwiseFlagTest.php
@@ -4,47 +4,47 @@ namespace ModHelper\Tests;
 
 class MockBitwiseFlag extends \ModHelper\BitwiseFlag
 {
-  const FLAG_REGISTERED = 1; // BIT #1 of $flags has the value 1
-  const FLAG_ACTIVE = 2;     // BIT #2 of $flags has the value 2
-  const FLAG_MEMBER = 4;     // BIT #3 of $flags has the value 4
-  const FLAG_ADMIN = 8;      // BIT #4 of $flags has the value 8
+    const FLAG_REGISTERED = 0x1; // BIT #1 of $flags has the value 1
+    const FLAG_ACTIVE = 0x2;         // BIT #2 of $flags has the value 2
+    const FLAG_MEMBER = 0x4;         // BIT #3 of $flags has the value 4
+    const FLAG_ADMIN = 0x8;            // BIT #4 of $flags has the value 8
 
-  public function isRegistered(){
-    return $this->isFlagSet(self::FLAG_REGISTERED);
-  }
+    public function isRegistered(){
+        return $this->isFlagSet(self::FLAG_REGISTERED);
+    }
 
-  public function isActive(){
-    return $this->isFlagSet(self::FLAG_ACTIVE);
-  }
+    public function isActive(){
+        return $this->isFlagSet(self::FLAG_ACTIVE);
+    }
 
-  public function isMember(){
-    return $this->isFlagSet(self::FLAG_MEMBER);
-  }
+    public function isMember(){
+        return $this->isFlagSet(self::FLAG_MEMBER);
+    }
 
-  public function isAdmin(){
-    return $this->isFlagSet(self::FLAG_ADMIN);
-  }
+    public function isAdmin(){
+        return $this->isFlagSet(self::FLAG_ADMIN);
+    }
 
-  public function setRegistered($value){
-    $this->setFlag(self::FLAG_REGISTERED, $value);
-  }
+    public function setRegistered($value){
+        $this->setFlag(self::FLAG_REGISTERED, $value);
+    }
 
-  public function setActive($value){
-    $this->setFlag(self::FLAG_ACTIVE, $value);
-  }
+    public function setActive($value){
+        $this->setFlag(self::FLAG_ACTIVE, $value);
+    }
 
-  public function setMember($value){
-    $this->setFlag(self::FLAG_MEMBER, $value);
-  }
+    public function setMember($value){
+        $this->setFlag(self::FLAG_MEMBER, $value);
+    }
 
-  public function setAdmin($value){
-    $this->setFlag(self::FLAG_ADMIN, $value);
-  }
+    public function setAdmin($value){
+        $this->setFlag(self::FLAG_ADMIN, $value);
+    }
 
-	function __toStringt()
-	{
-		return $this->flags;
-	}
+    function __toStringt()
+    {
+        return $this->flags;
+    }
 }
 
 class BitwiseFlagTest extends \PHPUnit_Framework_TestCase
@@ -55,9 +55,9 @@ class BitwiseFlagTest extends \PHPUnit_Framework_TestCase
     {
         $this->loader = new MockBitwiseFlag;
 
-		$this->loader->setRegistered(true);
-		$this->loader->setActive(true);
-		$this->loader->setMember(true);
+        $this->loader->setRegistered(true);
+        $this->loader->setActive(true);
+        $this->loader->setMember(true);
     }
 
     public function testInitialSet()
@@ -77,7 +77,6 @@ class BitwiseFlagTest extends \PHPUnit_Framework_TestCase
 
     public function testRawBits()
     {
-        $this->assertSame(7, $this->loader);
+        $this->assertSame(0x7, (string) $this->loader);
     }
-
 }
