64457d083877f3d38e705acb788710bcb950933c
John Rayes <live627@gmail.com>
2016-03-18 16:09:08 -0700

    Create HooksTest.php

---
+++ tests/HooksTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace ModHelper\Tests;
+
+class MockHooks extends \ModHelper\Hooks
+{
+    public function getHooks()
+    {
+        return $this->collection;
+    }
+}
+
+class HooksTest extends \PHPUnit_Framework_TestCase
+{
+    protected $l;
+
+    protected function setUp()
+    {
+        $this->l = new MockHooks;
+
+        $this->l->add(
+            'Foo',
+            '/vendor/foo'
+        );
+
+        $this->l->add(
+            'BarDoom',
+            '/vendor/foo.bardoom'
+        );
+
+    }
+
+    public function testExistingHook()
+    {
+        $expect = array(
+            'Foo',
+            '/vendor/foo'
+        );
+        $this->assertContains($expect, $this->l->getHooks());
+
+        $expect = array(
+            'BarDoom',
+            '/vendor/foo.bardoom'
+        );
+        $this->assertContains($expect, $this->l->getHooks());
+    }
+
+    public function testMissingHook()
+    {
+        $expect = array(
+            'Baz Dib',
+            '/vendor/baz.dib'
+        );
+        $this->assertNotContains($expect, $this->l->getHooks());
+    }
+
+    public function testHookCount()
+    {
+        $this->assertCount(2, $this->l->getHooks());
+    }
+}
