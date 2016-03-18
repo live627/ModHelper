b39a454ea433306e248180062df4bac6712c8543
John Rayes <live627@gmail.com>
2016-03-18 14:58:44 -0700

    Update NonceTest.php

--- tests/NonceTest.php
+++ tests/NonceTest.php
@@ -37,6 +37,12 @@ class NonceTest extends \PHPUnit_Framework_TestCase
 
     public function testAttack()
     {
+        if (session_status() === PHP_SESSION_DISABLED) {
+            throw new RuntimeException('PHP sessions are disabled');
+        }
+        if (session_status() === PHP_SESSION_ACTIVE) {
+            throw new RuntimeException('Failed to start the session: already started by PHP.');
+        }
             $_SERVER['REMOTE_ADDR'] = 'ModHelper Test Suite';
             $_SERVER['HTTP_USER_AGENT'] = 'ModHelper';
             $hash = $this->loader->generate();
