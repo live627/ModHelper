55e8caa3ac56a212b66327eaef3de2bd236d814f
John Rayes <live627@gmail.com>
2016-03-24 18:37:49 -0700

    Create YamlParserException.php

---
+++ src/Exceptions/YamlParserException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ModHelper\Exceptions;
+
+use \Symfony\Component\Yaml\Exception\ParseException;
+
+/**
+ * @copyright Copyright (c) 2015 John Rayes
+ * @license http://opensource.org/licenses/MIT MIT
+ *
+ * @package ModHelper
+ * @version 2.0
+ */
+class YamlParserException extends ParseException {}
