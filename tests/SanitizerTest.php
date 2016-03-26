a0da49bd1d0c749bb21261b5c262f00a66adbbfc
John Rayes <live627@gmail.com>
2016-03-26 02:53:24 -0700

    Create SanitizerTest.php

---
+++ tests/SanitizerTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace ModHelper\Tests;
+
+use \ModHelper\Sanitizer
+
+$smcFunc['htmltrim'] = trim;
+$smcFunc['htmlspecialchars'] = htmlspecialchars;
+$smcFunc['substr'] = substr;
+
+class SanitizerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testNotInt()
+    {
+        Sanitizer::sanitizeInt([0x10]);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testMinNotInt()
+    {
+        Sanitizer::sanitizeInt(0x10, [0x0]);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testMaxNotInt()
+    {
+        Sanitizer::sanitizeInt(0x10, 0x0, [0x8]);
+    }
+
+    /**
+     * @expectedException \RangeException
+     */
+    public function testTooBig()
+    {
+        Sanitizer::sanitizeInt(0x10, 0x0, 0x8);
+    }
+
+    /**
+     * @expectedException \RangeException
+     */
+    public function testTooSmall()
+    {
+        Sanitizer::sanitizeInt(0x1, 0x4, 0x8);
+    }
+
+    public function testTooBig()
+    {
+        $this->assertSame(0x4, Sanitizer::sanitizeInt(0x4, 0x0, 0x8));
+    }
+
+    public function testEmail()
+    {
+        $this->assertTrue(Sanitizer::sanitizeEmail('live627@gmail.com'));
+    }
+
+    public function testText()
+    {
+        $this->assertSame('1&amp;2', Sanitizer::sanitizeText('1&2'));
+    }
+}
