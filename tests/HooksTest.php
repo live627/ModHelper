2c028f8dc5f086f36b88eda20b0e1ebe263e63ae
John Rayes <live627@gmail.com>
2016-04-03 16:53:25 -0700

    Create HooksTest.php

---
+++ tests/HooksTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace ModHelper\Tests;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class HooksTest extends \PHPUnit_Framework_TestCase
+{
+    protected $l;
+
+    protected function setUp()
+    {
+        include __DIR__ . '/func.php';
+        $container = new ContainerBuilder();
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__));
+        $loader->load('services.yml');
+        $this->l = $container->get('hooks');
+        $this->l->execute(true);
+    }
+
+    public function testExistingHook()
+    {
+        global $modSettings;
+
+        $expect = array(
+            'Foo'=>
+            '/vendor/foo'
+        );
+        $this->assertContains($expect, $modSettings);
+
+        $expect = array(
+            'BarDoom'=>
+            '/vendor/foo.bardoom',
+        );
+        $this->assertContains($expect, $modSettings);
+    }
+
+    public function testMissingHook()
+    {
+        global $modSettings;
+
+        $expect = array(
+            'Baz Dib'=>
+            '/vendor/baz.dib'
+        );
+        $this->assertNotContains($expect, $modSettings);
+    }
+
+    public function testHookCount()
+    {
+        global $modSettings;
+
+        $this->assertCount(2, $modSettings);
+    }
+}
