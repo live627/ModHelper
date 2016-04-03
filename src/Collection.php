6b28ec130b162f3306e156ff132ac07e0ac74271
John Rayes <live627@gmail.com>
2016-04-03 00:20:49 -0700

    Create Collection.php

---
+++ src/Collection.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ModHelper;
+
+/**
+ * @package ModHelper
+ * @since 1.0
+ */
+class Collection extends \ArrayObject
+{
+    /**
+     * Construct the object.
+     *
+     * @access public
+     * @param array
+     * @abstracting \ArrayObject
+     * @since 2.0
+     */
+    function __construct(array $array=[]){
+        parent::__construct($array,parent::STD_PROP_LIST|parent::ARRAY_AS_PROPS);
+    }
+}
