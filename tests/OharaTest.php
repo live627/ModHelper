8b86e9e7b0fac5e9e5b821fc797bd6f876975a9e
John Rayes <live627@gmail.com>
2016-04-10 20:27:00 -0700

    Create OharaTest.php

---
+++ tests/OharaTest.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @param string $template_name
+ */
+function loadLanguage($template_name)
+{
+    global $txt;
+
+    $txt['months_title'] = 'Months';
+    $txt['MockOhara_months'] = array(1 => 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+}
+
+class MockOhara extends ModHelper\Ohara
+{
+    public $name = __CLASS__;
+}
+
+class MockSukiOhara extends Suki\Ohara
+{
+    public $name = __CLASS__;
+
+    public function __construct()
+    {
+        $this->setRegistry();
+    }
+}
+
+class OharaTest extends PHPUnit_Framework_TestCase
+{
+    protected $loader;
+    protected $o;
+
+    protected function setUp()
+    {
+        $this->loader = new MockOhara;
+        $this->o = new MockSukiOhara;
+    }
+
+    public function testText()
+    {
+        $actual = $this->loader->text('months')[2];
+        $this->assertSame('February', $actual)
+        $actual = $this->o->text('months')[2];
+        $this->assertSame('February', $actual)
+        $actual = $this->loader->text('months_title');
+        $this->assertSame('Months', $actual)
+        $actual = $this->o->text('months_title');
+        $this->assertFalse($actual)
+        $actual = $this->loader->text('days_title');
+        $this->assertFalse($actual)
+        $actual = $this->o->text('days_title');
+        $this->assertFalse($actual)
+    }
+}
