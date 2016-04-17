aeced3673081f7badab3696918426fb6bdbdcb81
John Rayes <live627@gmail.com>
2016-04-16 18:33:17 -0700

    Add enums

---
+++ src/Enum.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace ModHelper;
+
+/**
+ * @copyright Copyright (c) 2015 John Rayes
+ * @license http://opensource.org/licenses/MIT MIT
+ *
+ * @package ModHelper
+ * @version 2.0
+ */
+
+abstract class Enum
+{
+    protected $value;
+    protected static $cache = [];
+
+    /**
+     * @param mixed $value
+     *
+     * @throws \UnexpectedValueException if incompatible type is given.
+     */
+    public function __construct($value)
+    {
+        if (!$this->isValid($value)) {
+            throw new \UnexpectedValueException("Value '$value' is not part of the enum " . static::class);
+        }
+
+        $this->value = $value;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * Returns the enum key (i.e. the constant name).
+     *
+     * @return mixed
+     */
+    public function getKey()
+    {
+        return array_search($this->value, static::toArray(), true);
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return (string) $this->value;
+    }
+
+    /**
+     * Returns the names (keys) of all constants in the Enum class
+     *
+     * @return array
+     */
+    public static function keys()
+    {
+        return array_keys(static::toArray());
+    }
+
+    /**
+     * Returns instances of the Enum class of all Enum constants
+     *
+     * @return array Constant name in key, Enum instance in value
+     */
+    public static function values()
+    {
+        $values = array();
+
+        foreach (static::toArray() as $key => $value) {
+            $values[$key] = new static($value);
+        }
+
+        return $values;
+    }
+
+    /**
+     * Returns all possible values as an array
+     *
+     * @return array Constant name in key, constant value in value
+     */
+    public static function toArray()
+    {
+        $class = static::class;
+        if (!array_key_exists($class, static::$cache)) {
+            static::$cache[$class] = (new \ReflectionClass)($class)->getConstants();
+        }
+
+        return static::$cache[$class];
+    }
+
+    /**
+     * Check if is valid enum value
+     *
+     * @param $value
+     *
+     * @return bool
+     */
+    public static function isValid($value)
+    {
+        return in_array($value, static::toArray(), true);
+    }
+
+    /**
+     * Check if is valid enum key
+     *
+     * @param $key
+     *
+     * @return bool
+     */
+    public static function isValidKey($key)
+    {
+        return isset(static::toArray()[$key]);
+    }
+
+    /**
+     * Returns a value when called statically like so: MyEnum::SOME_VALUE() given SOME_VALUE is a class constant
+     *
+     * @param string $name
+     * @param array  $arguments
+     *
+     * @return static
+     * @throws \BadMethodCallException
+     */
+    public static function __callStatic($name, $arguments)
+    {
+        $array = static::toArray();
+        if (isset($array[$name])) {
+            return new static($array[$name]);
+        }
+
+        throw new \BadMethodCallException("No static method or enum constant '$name' in class " . static::class);
+    }
+}
---
+++ tests/EnumTest.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace ModHelper\Tests;
+
+class Thing extends \ModHelper\Enum
+{
+    const FOO = "foo";
+    const BAR = "bar";
+    const NUMBER = 42;
+    const PROBLEMATIC_NUMBER = 0;
+    const PROBLEMATIC_NULL = null;
+    const PROBLEMATIC_EMPTY_STRING = '';
+    const PROBLEMATIC_BOOLEAN_FALSE = false;
+}
+
+class EnumTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetValue()
+    {
+        $value = new Thing(Thing::FOO);
+        $this->assertEquals(Thing::FOO, $value->getValue());
+
+        $value = new Thing(Thing::BAR);
+        $this->assertEquals(Thing::BAR, $value->getValue());
+
+        $value = new Thing(Thing::NUMBER);
+        $this->assertEquals(Thing::NUMBER, $value->getValue());
+    }
+
+    /**
+     * @dataProvider getKeyProvider
+     */
+    public function testGetKey($value, $expected)
+    {
+        $this->assertSame($expected, Thing::getKey($value));
+    }
+
+    public function getKeyProvider()
+    {
+        return array(
+            array('foo', 'FOO'),
+            array(0, 'PROBLEMATIC_NUMBER'),
+            array(null, 'PROBLEMATIC_NULL'),
+            array('', 'PROBLEMATIC_EMPTY_STRING'),
+            array(false, 'PROBLEMATIC_BOOLEAN_FALSE'),
+            array('bar I do not exist', false),
+            array(array(), false),
+        );
+    }
+
+    /**
+     * @dataProvider invalidValueProvider
+     */
+    public function testCreatingEnumWithInvalidValue($value)
+    {
+        $this->setExpectedException(
+            '\UnexpectedValueException',
+            'Value \'' . $value . '\' is not part of the enum ModHelper\Tests\Enum\Thing'
+        );
+
+        new Thing($value);
+    }
+
+    public function invalidValueProvider() {
+        return array(
+            "string" => array('test'),
+            "int" => array(1234),
+        );
+    }
+
+    /**
+     * @dataProvider toStringProvider
+     */
+    public function testToString($expected, $enumObject)
+    {
+        $this->assertSame($expected, (string) $enumObject);
+    }
+
+    public function toStringProvider() {
+        return array(
+            array(Thing::FOO, new Thing(Thing::FOO)),
+            array(Thing::BAR, new Thing(Thing::BAR)),
+            array((string) Thing::NUMBER, new Thing(Thing::NUMBER)),
+        );
+    }
+
+    public function testKeys()
+    {
+        $values = Thing::keys();
+        $expectedValues = array(
+            "FOO",
+            "BAR",
+            "NUMBER",
+            "PROBLEMATIC_NUMBER",
+            "PROBLEMATIC_NULL",
+            "PROBLEMATIC_EMPTY_STRING",
+            "PROBLEMATIC_BOOLEAN_FALSE",
+        );
+
+        $this->assertSame($expectedValues, $values);
+    }
+
+    public function testValues()
+    {
+        $values = Thing::values();
+        $expectedValues = array(
+            "FOO"                       => new Thing(Thing::FOO),
+            "BAR"                       => new Thing(Thing::BAR),
+            "NUMBER"                    => new Thing(Thing::NUMBER),
+            "PROBLEMATIC_NUMBER"        => new Thing(Thing::PROBLEMATIC_NUMBER),
+            "PROBLEMATIC_NULL"          => new Thing(Thing::PROBLEMATIC_NULL),
+            "PROBLEMATIC_EMPTY_STRING"  => new Thing(Thing::PROBLEMATIC_EMPTY_STRING),
+            "PROBLEMATIC_BOOLEAN_FALSE" => new Thing(Thing::PROBLEMATIC_BOOLEAN_FALSE),
+        );
+
+        $this->assertEquals($expectedValues, $values);
+    }
+
+    public function testToArray()
+    {
+        $values = Thing::toArray();
+        $expectedValues = array(
+            "FOO"                   => Thing::FOO,
+            "BAR"                   => Thing::BAR,
+            "NUMBER"                => Thing::NUMBER,
+            "PROBLEMATIC_NUMBER"    => Thing::PROBLEMATIC_NUMBER,
+            "PROBLEMATIC_NULL"      => Thing::PROBLEMATIC_NULL,
+            "PROBLEMATIC_EMPTY_STRING"    => Thing::PROBLEMATIC_EMPTY_STRING,
+            "PROBLEMATIC_BOOLEAN_FALSE"    => Thing::PROBLEMATIC_BOOLEAN_FALSE,
+        );
+
+        $this->assertSame($expectedValues, $values);
+    }
+
+    public function testStaticAccess()
+    {
+        $this->assertEquals(new Thing(Thing::FOO), Thing::FOO());
+        $this->assertEquals(new Thing(Thing::BAR), Thing::BAR());
+        $this->assertEquals(new Thing(Thing::NUMBER), Thing::NUMBER());
+    }
+
+    /**
+     * @expectedException \BadMethodCallException
+     * @expectedExceptionMessage No static method or enum constant 'UNKNOWN' in class
+     *                           UnitTest\ModHelper\Enum\Thing
+     */
+    public function testBadStaticAccess()
+    {
+        Thing::UNKNOWN();
+    }
+
+    /**
+     * @dataProvider isValidProvider
+     */
+    public function testIsValid($value, $isValid)
+    {
+        $this->assertSame($isValid, Thing::isValid($value));
+    }
+
+    public function isValidProvider() {
+        return array(
+            array('foo', true),
+            array(42, true),
+            array(null, true),
+            array(0, true),
+            array('', true),
+            array(false, true),
+            array('baz', false)
+    );
+    }
+
+    public function testIsValidKey()
+    {
+        $this->assertTrue(Thing::isValidKey('FOO'));
+        $this->assertFalse(Thing::isValidKey('BAZ'));
+    }
+}
